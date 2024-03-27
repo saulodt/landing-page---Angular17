f935e1ca757c98eb2d7ad7efdd87fe2d1a9201c1
Saulo de Társio <saulo.d.t@gmail.com>
2024-03-27 10:02:48 -0300

    landing page angular17

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "",
+        component: HomeComponent
+    }
+
+];
+
